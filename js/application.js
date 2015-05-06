6c48af4580ed6ff7ffc1fe3dd8ca36465cfd86e7
Grzegorz Zdanowski <grzegorz@noflash.pl>
2015-05-06 19:28:12 +0200

    Added prompter start shortcut.

--- js/application.js
+++ js/application.js
@@ -6,8 +6,22 @@ var Prompter = function () {
     this.textarea = new Textarea(this.settingsStorage);
 
     this.settings = this.settingsStorage.extend('prompter', {
-        text: "Welcome to OpenPrompter!\n\nHave you ever thought how news station presenters memorize all that information? Well, they don't - it's just teleprompter magic.\n\nTelemprompter is a device displaying scrolling text, read by someone in front of camera. Typicaly teleprompters are specialized, and rather expensive devices used by pros, but any tablet and piece of glass works as well.\nThe only missing part is application - simple, reliable, and of course free. That's why OpenPromtper born - use it anytime and anywhere.\n\nRemember to adjust speed and sizes to your needs. You can also change colors."
+        text: "Welcome to OpenPrompter!\n\nHave you ever thought how news station presenters memorize all that information? Well, they don't - it's just teleprompter magic.\n\nTelemprompter is a device displaying scrolling text, read by someone in front of camera. Typicaly teleprompters are specialized, and rather expensive devices used by pros, but any tablet and piece of glass works as well.\nThe only missing part is application - simple, reliable, and of course free. That's why OpenPromtper born - use it anytime and anywhere.\n\nRemember to adjust speed and sizes to your needs. You can also change colors.",
+        preloader: true
     });
+};
+
+Prompter.prototype.start = function() {
+    var _self = this;
+
+    if(this.settings.preloader) {
+        this.preloader.start(function() {
+           _self.textarea.start();
+        });
+
+    } else {
+        this.textarea.start();
+    }
 };
 
 $(function () {
